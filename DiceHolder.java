72de164270aae931fab7f18a569a56dfd7657c50
LucasYoung35 <45399073+LucasYoung35@users.noreply.github.com>
2018-12-05 10:20:40 -0500

    addDie and toString
    added addDie and toString methods

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
 public class DiceHolder{
+
+	private ArrayList<Die> holder;
+
+
+	public int addDie(Die die){
+		if(holder.size()<6){
+			holder.add(die);
+			return 1;
+			}
+		else{
+			return -1;
+			}
+		}
+
+	public String toString(){
+		String s="";
+		for(int i=0;i<holder.size();i++){
+			s+=holder.get(i)+"\n";
+			}
+		}
+
+
 }
