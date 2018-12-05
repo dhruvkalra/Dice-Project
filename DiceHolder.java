59a9f3c0463ca19256ab8e0e71f50166f6c0c6f4
dhruvkalra <dhruvkalra@hotmail.com>
2018-12-05 10:22:48 -0500

    Update DiceHolder.java

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
 public class DiceHolder{
+	public DiceHolder{
+		holder = new ArrayList<>();
+	}
+	public void shake(){
+		for(int i=0;i<holder.size();i++){
+			holder.set(i, ((int)(Math.random()*6)+1));
+		}
+	}
 }
