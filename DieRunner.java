f67d9cf87b31c52372885ceaa8280fde21bcc29b
praneetk07 <praneetk07@gmail.com>
2018-12-05 10:41:11 -0500

    finished Test 1 for the runner class

--- DieRunner.java
+++ DieRunner.java
@@ -1,7 +1,20 @@
 public class DieRunner{
 	public static void main(String[] args){
-		Die x = new Die(18);
-		System.out.println(x);
+		Die x = new Die(6);
+		Die y = new Die(6);
+
+		int counter = 0;
+		while(x.getValue()!=1 || y.getValue()!=1){
+			counter ++;
+			x.roll();
+			y.roll();
+			System.out.println("Run: " + counter);
+			System.out.println("Die 1: " +x);
+			System.out.println("Die 2: " +y);
+			System.out.println();
+
+		}
+		System.out.println("Number of tries to get snake eyes: " + counter);
 
 
 	}
